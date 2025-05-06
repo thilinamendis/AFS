a2c99cd17cbe0203d310492f8d8cf16830b0cad2
Kavindu Shavinka <kavindushavinka022@gmail.com>
2025-05-07 00:04:41 +0530

    feat: integrate About component into App for enhanced informational display

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import Footer from './components/Footer'
+import About from './components/About'
 
 
 const App = () => {
@@ -9,6 +10,7 @@ const App = () => {
     <main className='overflow-x-hidden'>
       <Navbar/>
       <Hero />
+      <About />
       <Footer />
     </main>
   )
