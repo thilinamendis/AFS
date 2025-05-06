09f85d6d773d197af0bf92a44331b7bb2f67a2b1
Kavindu Shavinka <kavindushavinka022@gmail.com>
2025-05-06 15:52:19 +0530

    feat: enhance Navbar component with toggle functionality and icons

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
+
 
 const Navbar = () => {
+
+  const [nav,setNav] = useState(false);
+
+  const handleNav = () => {
+    setNav(!nav);
+  }
+
   return (
-    <div>Navbar</div>
+    <div>
+
+    </div>
   )
 }
 
