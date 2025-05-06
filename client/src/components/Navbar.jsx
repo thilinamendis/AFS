398d7ceb8536a82b509ee3b57899355d8720e401
Kavindu Shavinka <kavindushavinka022@gmail.com>
2025-05-06 15:57:33 +0530

    feat: add navigation items to Navbar component

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -10,9 +10,20 @@ const Navbar = () => {
     setNav(!nav);
   }
 
-  return (
-    <div>
+  const navItems = [
+    {id:1, text: 'Home'},
+    {id:2, text: 'About'},
+    {id:3, text: 'Services'},
+    {id:4, text: 'Projects'},
+    {id:5, text: 'Contact'}
+  ]
 
+
+
+  return (
+    <div className='bg-black flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-black'>
+      {/* logo */}
+      
     </div>
   )
 }
